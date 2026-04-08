035858fdfd57fa78a80057ade50c8794bf757366
ReygaDirga <reygadirg@gmail.com>
2026-04-08 11:04:52 +0700

    session2

---
+++ session2/resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to the Login Page</h1>
+    {{-- versi 1 --}}
+    <button><a href="/register">Register</a></button>
+    {{-- versi 2 --}}
+    <button><a href="{{ route('register.view') }}">Register</a></button>
+    {{-- versi 3 --}}
+    <button><a href="{{ url('/register') }}">Register</a></button>
+    <button><a href="{{ url('/about') }}">About</a></button>
+</body>
+</html>
